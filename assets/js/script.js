e5a854631bde91e978dbdd8da7ebdbe82d4b608c
ccarabine <ccarabine12@gmail.com>
2021-08-22 16:17:04 +0000

    Add code to prevent bug - if click twice on the same card, match will equal true and it will remove the event listerner therefore disable the card

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,13 @@ let firstClick;
 let secondClick;
 
 function selectCard() {
-    if (boardDisabled) return; // if boardDisabled =true return else disable board and enable it after the cards have been selected
-    
+    if (boardDisabled) return; // if boardDisabled =true return so the rest doesnt get executed else disable board and enable it (in the match function) after the cards have been selected
+    if (this === firstClick) return; // if we click twice on a the same card, match will equal to true and it will remove the event listener so have incorporated this line of code
+    //if its the first click then the (this) varaible holds the first card but the first card varaible is still unset so the condition is  going to evaluate to false and the function wil be executed normally.
+    //if its is the second car clicked then the (this) varaible holds the second card, if this equal first card then it will return from the function
+
+    if it equals to the firstclick varaible then it returns,
+    if it the second click this is the first click then false
     this.classList.toggle('select'); //  this keyword relates to '.card' class, if the '.select' class is there remove it, if not add it 
 
     if (!selectedCard) { // if selectedcard equals false then the player has selected the first card
@@ -28,7 +33,7 @@ function match() {
         firstClick.removeEventListener('click', selectCard); // disabled the user from clickiing the matched pair of cars
         secondClick.removeEventListener('click', selectCard);
     } else { //Not a match,  set a delay 1000milseconds so we can see the 2nd clicked card, turn back over the cards(rotate by 180oc remove the classes '.select')
-        disabledBoard = true;
+        disabledBoard = true; //unlock the board and wait until they have turned back over
         setTimeout(() => {
             firstClick.classList.remove('select');
             secondClick.classList.remove('select');
