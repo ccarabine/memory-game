25c3daa7e6dabbe449cefdbc406fd4098a2c5e80
ccarabine <ccarabine12@gmail.com>
2021-08-22 15:03:28 +0000

    Add selectCard function to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
 const cards = document.querySelectorAll('.card');
+let selectedCard = false;
+let firstClick;
+let secondClick;
 
 function selectCard() {
-       this.classList.toggle('select');    //  this keyword relates to '.card' class, if the '.select' class is there remove it, if not add it 
-           }
+    this.classList.toggle('select'); //  this keyword relates to '.card' class, if the '.select' class is there remove it, if not add it 
+  
+    if (!selectedCard) { // if selectedcard equals false then the player has selected the first card
+        selectedCard = true;
+        firstClick = this;
+        console.log({selectedCard, firstClick});
+    } else {  // if selectedcard equals true then the player has selected the second card
+        selectedCard = false;
+        secondClick = this;
+        console.log({selectedCard, secondClick});
+    }
+}
 
 cards.forEach(card => card.addEventListener('click', selectCard)); // loop for each card click invoke the select card function
