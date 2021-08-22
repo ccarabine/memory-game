1eccc4a6ea9a2c4d73a8b8d5b2b90614c754b50e
ccarabine <ccarabine12@gmail.com>
2021-08-22 13:42:56 +0000

    Add select card function, event listener on clickevent  and styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,11 @@
-console.log('js is  working');
+const cards = document.querySelectorAll('.card');
 
+function selectCard() {
+   
+    this.classList.toggle('select');    //  this relates to card class, if it is true add the flip card, if false set it to turn  
+  
+    
+    
+      }
+
+cards.forEach(card => card.addEventListener('click', selectCard)); // loop for each card click envoke the select card function
