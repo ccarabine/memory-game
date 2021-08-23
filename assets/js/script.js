4df03d00b2b1c1301b1f5e2eeba60ab026d20aac
ccarabine <ccarabine12@gmail.com>
2021-08-23 18:58:37 +0000

    Add startgame modal, modal styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,4 +61,31 @@ function resetBoard() {
     firstClick = null;
     secondClick = null;
 }
+//MODAL CODE
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal - might not need
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+//OPENS AT THE BEGINING
+  modal.style.display = "block";
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 cards.forEach(card => card.addEventListener('click', selectCard)); // loop for each card click invoke the select card function
