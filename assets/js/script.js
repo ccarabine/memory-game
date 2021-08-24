dc9562648ce68dbd2446ac3bf7c2db6263862153
ccarabine <ccarabine12@gmail.com>
2021-08-24 18:48:26 +0000

    Add function docstrings to every function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,54 @@
-const cards = document.querySelectorAll('.card');
 
-// Get the modal
+
+//Modals
 var newGameModal = document.getElementById("newGameModal");
-
 var rulesModal = document.getElementById("rulesModal");
 var confirmModal = document.getElementById("confirmModal");
-// newGamebutton 
 var newGameBtn = document.getElementById("newGameBtn");
 var rulesBtn = document.getElementById("rulesBtn");
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 var closerules = document.getElementsByClassName("closerules")[0];
-//declare move varaible
+
+
+//Get Elements
+const cards = document.querySelectorAll('.card');
 let countermoves = document.getElementById("moves");
 let countermatchedpairs = document.getElementById("matchedpairs");
-let moves = 0;
-let matchedpairs = 0;
-
 let resultmoves = document.getElementById("resultmoves");
 let resultscore = document.getElementById("resultscore");
+
+//declare Variables
+let moves = 0;
+let matchedpairs = 0;
 let score = 0;
 let selectedCard = false;
 let boardDisabled = false;
 let firstClick;
 let secondClick;
 
+/**
+ * Function SelectCard 
+ * 1.  boardDisabled: // if boardDisabled =true return so the rest doesnt get executed else disable board and enable it (in the match function) after the cards have been selected
+ * 2. if statement to fix bug :if user clicks twice on a the same card, match will equal to true and it will remove the event listener so have incorporated this line of code
+ *    solution: if its the first click then the (this) varaible holds the first card but the first card varaible is still unset so the condition is  going to evaluate to false and the function wil be executed normally.
+ *   if its is the second card clicked then the (this) varaible holds the second card, if this equal first card then it will remove the "select" class therefore turning the card back over
+ * 3. Turn the card over : this keyword relates to '.card' class, if the '.select' class is there remove it, if not add it 
+ * 4. if selectedcard equals false then the player has selected the first card
+ * 5. if selectedcard equals true then the player has selected the second card
+ */
 function selectCard() {
-    if (boardDisabled) return; // if boardDisabled =true return so the rest doesnt get executed else disable board and enable it (in the match function) after the cards have been selected
-    if (this === firstClick) {
-        this.classList.remove('select');  // if the first click equals second on the same card, remove the select class so it turns back over
+    if (boardDisabled) return; //1.
+    if (this === firstClick) { //2.
+        this.classList.remove('select');
         return;
     }
-    // if we click twice on a the same card, match will equal to true and it will remove the event listener so have incorporated this line of code
-    //if its the first click then the (this) varaible holds the first card but the first card varaible is still unset so the condition is  going to evaluate to false and the function wil be executed normally.
-    //if its is the second car clicked then the (this) varaible holds the second card, if this equal first card then it will return from the function
+    this.classList.add('select'); // 3.
 
-
-    this.classList.add('select'); //  this keyword relates to '.card' class, if the '.select' class is there remove it, if not add it 
-
-    if (!selectedCard) { // if selectedcard equals false then the player has selected the first card
+    if (!selectedCard) { // 4.
         selectedCard = true;
         firstClick = this;
         return;
-    } else { // if selectedcard equals true then the player has selected the second card
+    } else { // 5.
         selectedCard = false;
         secondClick = this;
         moveCounter();
@@ -50,36 +56,45 @@ function selectCard() {
     }
 }
 
-
+/**
+ * Function - Match
+ * 1.if first card equals second card then its a match, remove event listeners on first click and second click card
+ * which will disabled the user from clickiing the matched pair of cars
+ * 2. Call matchPairsCounter Function and resetBoard
+ * 3. If match pairs equal 6, then call finishGame function
+ * 4. Unlock the board and wait until they have turned back over
+ * 5. Not a match,  set a delay 1000milseconds so we can see the 2nd clicked card, turn back over the cards(rotate by 180oc remove the classes '.select')
+ */
 function match() {
-    if (firstClick.dataset.cardtype === secondClick.dataset.cardtype) { // if first card equals second card then its a match
-        firstClick.removeEventListener('click', selectCard); // disabled the user from clickiing the matched pair of cars
-        secondClick.removeEventListener('click', selectCard);
-        matchPairsCounter();
+    if (firstClick.dataset.cardtype === secondClick.dataset.cardtype) { // 1. 
+        firstClick.removeEventListener('click', selectCard);  
+        secondClick.removeEventListener('click', selectCard); 
+        matchPairsCounter(); //2.
         resetBoard();
-        //  console.log(cards);
-
-        if (matchedpairs == 2) {
+        if (matchedpairs == 6) { //3.
             finishGame();
         }
-    } else { //Not a match,  set a delay 1000milseconds so we can see the 2nd clicked card, turn back over the cards(rotate by 180oc remove the classes '.select')
-        disabledBoard = true; //unlock the board and wait until they have turned back over
-        setTimeout(() => {
+    } else { //4.
+        disabledBoard = true; 
+        setTimeout(() => {//5. 
             firstClick.classList.remove('select');
             secondClick.classList.remove('select');
             resetBoard();
         }, 1000);
     }
 }
-
+/**
+ * Function - ShuffleCards, Reposition the order of the cards by order property and generating a random number to position it
+ */
 function shuffleCards() {
-
     for (let i = 0; i < cards.length; i++) {
         let position = Math.floor(Math.random() * 12);
         cards[i].style.order = position;
     }
 }
-
+/**
+ * Function - reset boards and resets the values of selectedCard, boardDisabled, firstClick and secondClick
+ */
 function resetBoard() {
 
     selectedCard = false;
@@ -87,26 +102,33 @@ function resetBoard() {
     firstClick = null;
     secondClick = null;
 }
-
+/**
+ * Function - increments moves variable by 1 and displays the result in the main window
+ */
 function moveCounter() {
     moves++
     countermoves.innerHTML = `Moves: ${moves}   `;
 }
-
+/**
+ * Function - increments matchpairs variable by 1 and displays the result in the main window
+ */
 function matchPairsCounter() {
     matchedpairs++
     countermatchedpairs.innerHTML = `Matched Pairs: ${matchedpairs} `;
 }
 
+/**
+ * Function - closes new game modal, calls reset game function and listens out for click on card, then calls select card function
+ */
 function playNewGame() {
-    //reset game
-    // When the user clicks on the button, close the modal and start game
     newGameModal.style.display = "none";
-
     resetGame();
     cards.forEach(card => card.addEventListener('click', selectCard)); // loop for each card click invoke the select card function
 }
 
+/**
+ * Function - Reset scores to zero and calls remove select and shuffle card functions
+ */
 function resetGame() {
     moves = 0;
     matchedpairs = 0;
@@ -116,10 +138,10 @@ function resetGame() {
     removeSelect();
     shuffleCards();
 }
+
 /**
  * Function - Removes '.select' from all cards, so they show the back card
  */
-
 function removeSelect() {
     let selectCards = Array.from(document.querySelectorAll('.select'));
 
@@ -128,13 +150,17 @@ function removeSelect() {
     })
 };
 
-
-
+/**
+ * This Function closes the finishgamemodal and displays the new game modal
+ */
 function rules() {
     newGameModal.style.display = "none";
     rulesModal.style.display = "block";
 }
 
+/**
+ * This Function closes the finishgamemodal and displays the new game modal
+ */
 function displayNewGameModal() {
     finishgamemodal.style.display = "none";
     newGameModal.style.display = "block";
@@ -160,24 +186,25 @@ function finishGame() {
     resultmoves.innerHTML = `You have made ${moves} moves`;
     resultscore.innerHTML = `Your score is ${score} moves`;
     finishgamemodal.style.display = "block";
-
 }
+
 /**
  *  This function allows the user clicks on restart Game, the confirmation opens modal 
  */
-
 function confirmDecision() {
     confirmmodal.style.display = "block";
 }
+
 /**
  * This function calls the new game function and closes the confirm modal  
  */
-
 function restartGame() {
     displayNewGameModal()
     confirmmodal.style.display = "none";
 }
-// Confirmation modal - click no and it closes the confirm modal and continues game
+/**
+ * Function Click no on the Confirmation modal and it closes the confirmation modal and continues game
+ */
 function continueGame() {
     confirmmodal.style.display = "none";
 }
